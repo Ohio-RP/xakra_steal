b2516ad63542f0c22a49c7197b6e01dd81d8a3c4
Santos <55027189+nihonnetwork@users.noreply.github.com>
2025-03-14 14:58:44 +0900

    Check is on duty

---
+++ client.lua
@@ -0,0 +1,282 @@
+local StealPrompt = {}
+
+local active_menu = false
+
+local VORPcore = exports.vorp_core:GetCore()
+
+TriggerEvent("menuapi:getData", function(call)
+    MenuData = call
+end)
+
+function StealPlayerPrompt(entity)
+    local group = UiPromptGetGroupIdForTargetEntity(entity, Citizen.ResultAsLong())
+    StealPrompt[entity] = UiPromptRegisterBegin()
+    UiPromptSetControlAction(StealPrompt[entity], Config.KeySteal)
+    local VarString = CreateVarString(10, 'LITERAL_STRING', T.StrPrompt)
+    UiPromptSetText(StealPrompt[entity], VarString)
+    UiPromptSetEnabled(StealPrompt[entity], true)
+    UiPromptSetVisible(StealPrompt[entity], true)
+    UiPromptSetHoldMode(StealPrompt[entity], 1000)
+    UiPromptSetGroup(StealPrompt[entity], group)
+    UiPromptRegisterEnd(StealPrompt[entity])
+end
+
+CreateThread(function()
+    while true do
+        local t = 500
+
+        local DataSteal = LocalPlayer.state.DataSteal
+
+        for _, v in pairs(GetNearbyPlayer()) do
+            if not active_menu and v.source ~= 0 and v.enable and not Player(v.source).state.Stealing then
+                t = 0
+
+                if not StealPrompt[v.ped] then
+                    StealPlayerPrompt(v.ped)
+                end
+
+                if PromptHasHoldModeCompleted(StealPrompt[v.ped]) then
+                    TriggerServerEvent('xakra_steal:OpenMenu', v)
+                    Wait(500)
+                end
+
+            elseif active_menu and not v.enable and DataSteal and DataSteal.source == v.source then
+                CloseStealMenu()
+            end
+        end
+
+        if active_menu and DataSteal and not DoesEntityExist(DataSteal.ped) then
+            CloseStealMenu()
+        end
+
+        Wait(t)
+    end
+end)
+
+function CloseStealMenu()
+    if LocalPlayer.state.DataSteal then
+        TriggerServerEvent('xakra_steal:Stealing', LocalPlayer.state.DataSteal.source, false)
+        LocalPlayer.state:set('DataSteal', nil, true)
+    end
+
+    ClearPedTasks(PlayerPedId())
+
+    TriggerServerEvent('xakra_steal:CloseInventory')
+    
+    active_menu = false
+    MenuData.CloseAll()
+end
+
+CreateThread(function()
+    while Config.HandsUpButton do
+        Wait(0)
+
+        if IsControlJustPressed(0, Config.HandsUpButton) and IsInputDisabled(0) and not IsEntityDead(PlayerPedId()) then
+            local AnimDict, AnimName = 'script_proc@robberies@shop@rhodes@gunsmith@inside_upstairs', 'handsup_register_owner'
+
+            if IsEntityPlayingAnim(PlayerPedId(), AnimDict, AnimName, 3) then
+                SetCurrentPedWeapon(PlayerPedId(), joaat('WEAPON_UNARMED'), true)
+                DisablePlayerFiring(PlayerPedId(), true)
+                ClearPedSecondaryTask(PlayerPedId())
+
+            else
+                SetCurrentPedWeapon(PlayerPedId(), joaat('WEAPON_UNARMED'), true)
+                DisablePlayerFiring(PlayerPedId(), true)
+
+                if not HasAnimDictLoaded(AnimDict) then
+                    RequestAnimDict(AnimDict)
+    
+                    while not HasAnimDictLoaded(AnimDict) do
+                        Wait(0)
+                    end
+                end    
+
+                TaskPlayAnim(PlayerPedId(), AnimDict, AnimName, 2.0, -1.0, -1, 31, 0, true, 0, false, 0, false)
+
+                RemoveAnimDict(AnimDict)
+            end
+        end
+    end
+end)
+
+CreateThread(function()
+    while Config.DisableSuicidePrompt do
+        local t = 500
+
+        if IsPedHogtied(PlayerPedId()) == 1 then
+            t = 0
+            DisableControlAction(0, joaat('INPUT_CARRIABLE_SUICIDE'), true)
+        end
+
+        Wait(t)
+    end
+end)
+
+RegisterNetEvent('xakra_steal:OpenMenu')
+AddEventHandler('xakra_steal:OpenMenu', function(CharacterMoney)
+    MenuData.CloseAll()
+
+    if LocalPlayer.state.DataSteal and LocalPlayer.state.DataSteal.isOnDuty and 
+    Config.OnDuty.CheckDutyStatus and not Config.OnDuty.AllowStealFromOnDuty then
+     VORPcore.NotifyObjective(T.CantStealFromOnDuty or "You cannot steal from on-duty personnel.", 4000)
+     active_menu = false
+     return
+    end
+
+    ClearPedTasksImmediately(PlayerPedId())
+    SetCurrentPedWeapon(PlayerPedId(), joaat('WEAPON_UNARMED'), true, 0, false, false)
+    TaskStartScenarioInPlace(PlayerPedId(), joaat("WORLD_HUMAN_CROUCH_INSPECT"), 0, true, false, false, false)
+
+    active_menu = true
+
+    local elements = {
+        {
+            label = T.Money .. ': ' .. CharacterMoney .. '$',
+            value = 'money',
+            desc = T.DescStealMoney
+        },
+        {
+            label = T.Inventory,
+            value = 'inventory',
+            desc = T.DescStealInventory
+        },
+    }
+
+    MenuData.Open('default', GetCurrentResourceName(), 'StealMenu', {
+        title = T.MenuTitle,
+        subtext = T.MenuSubtext,
+        align = Config.Align,
+        elements = elements,
+
+    }, function(data, menu)
+        if not LocalPlayer.state.DataSteal then
+            return
+        end
+
+        if data.current.value == 'money' then                                      --translate here same as the config
+            local myInput = {
+                type = 'enableinput',                                                -- dont touch
+                inputType = 'input',                                                 -- or text area for sending messages
+                button = T.Confirm,                                    -- button name
+                placeholder = T.AmountMoney,                           --placeholdername
+                style = 'block',                                                     --- dont touch
+                attributes = {
+                    inputHeader = T.Money,                             -- header
+                    type = 'text',                                                   -- inputype text, number,date.etc if number comment out the pattern
+                    pattern = '[0-9.]{1,10}',                                        -- regular expression validated for only numbers '[0-9]', for letters only [A-Za-z]+   with charecter limit  [A-Za-z]{5,20}     with chareceter limit and numbers [A-Za-z0-9]{5,}
+                    title = 'Wrong value',                                           -- if input doesnt match show this message
+                    style = 'border-radius: 10px; background-color: ; border:none;', -- style  the inptup
+                }
+            }
+
+            TriggerEvent('vorpinputs:advancedInput', json.encode(myInput), function(result)
+                local number = tonumber(result)
+
+                if number and number <= CharacterMoney then
+                    TriggerServerEvent('xakra_steal:StealMoney', LocalPlayer.state.DataSteal.source, number)
+
+                    CharacterMoney = CharacterMoney - number
+                    menu.setElement(1, 'label', T.Money .. ': ' .. CharacterMoney .. '$')
+                    menu.refresh()
+                    
+                else
+                    VORPcore.NotifyObjective(T.TooMuchMoney, 4000)
+                end
+            end)
+        end
+
+        if data.current.value == 'inventory' then
+            TriggerServerEvent('xakra_steal:ReloadInventory', LocalPlayer.state.DataSteal.source)
+            TriggerServerEvent('xakra_steal:OpenInventory', LocalPlayer.state.DataSteal.source)
+        end
+
+    end, function(data, menu)
+        TriggerServerEvent('xakra_steal:Stealing', LocalPlayer.state.DataSteal.source, false)
+        LocalPlayer.state:set('DataSteal', nil, true)
+        ClearPedTasks(PlayerPedId())
+        active_menu = false
+        menu.close()
+    end)
+end)
+
+function GetNearbyPlayer()
+    local pcoords = GetEntityCoords(PlayerPedId())
+    local data_steal = {}
+
+    for _, id in pairs(GetActivePlayers()) do
+        local enable = false
+        local source = GetPlayerServerId(id)
+        local ped = GetPlayerPed(id)
+        
+        -- Check if player is on duty (only if enabled in config)
+        local isOnDuty = false
+        if Config.OnDuty.CheckDutyStatus then
+            isOnDuty = exports['ohio_duty']:isPlayerOnDuty(source)
+        end
+
+        if ped ~= PlayerPedId() and DoesEntityExist(ped) and GetDistanceBetweenCoords(pcoords, GetEntityCoords(ped), true) <= 2.5 then
+            -- Only consider stealing if player is not on-duty or if stealing from on-duty is allowed
+            if not isOnDuty or Config.OnDuty.AllowStealFromOnDuty then
+                if Config.StealHogtied and not IsEntityDead(ped) and IsPedHogtied(ped) == 1 and IsPedBeingHogtied(ped) == 0 then
+                    enable = true
+                end
+
+                if Config.Cuffed and not IsEntityDead(ped) and IsPedCuffed(ped) then
+                    enable = true
+                end
+
+                if Config.StealDead and IsEntityDead(ped) then
+                    enable = true
+                end
+
+                local isHandsUp = IsEntityPlayingAnim(ped, 'script_proc@robberies@shop@rhodes@gunsmith@inside_upstairs', 'handsup_register_owner', 3)
+
+                if Config.StealHandsUp and not IsEntityDead(ped) and isHandsUp then
+                    enable = true
+                end
+            end
+        end
+
+        data_steal[#data_steal + 1] = {
+            enable = enable,
+            source = source,
+            ped = ped,
+            isOnDuty = isOnDuty
+        }
+
+        if not enable and StealPrompt[ped] then
+            PromptDelete(StealPrompt[ped])
+            StealPrompt[ped] = nil
+        end
+    end
+
+    return data_steal
+end
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+
+    for _, v in pairs(StealPrompt) do
+        UiPromptDelete(v)
+    end
+
+    if LocalPlayer.state.DataSteal then
+        LocalPlayer.state:set('DataSteal', nil, true)
+    end
+
+    if LocalPlayer.state.Stealing then
+        LocalPlayer.state:set('Stealing', nil, true)
+    end
+
+    if active_menu then
+        MenuData.CloseAll()
+
+        if IsPedActiveInScenario(PlayerPedId()) then
+            ClearPedTasksImmediately(PlayerPedId())
+        end
+
+        TriggerServerEvent('xakra_steal:CloseInventory')
+    end
+end)
---
+++ server.lua
@@ -0,0 +1,276 @@
+local VORPcore = exports.vorp_core:GetCore()
+
+RegisterServerEvent('xakra_steal:OpenMenu')
+AddEventHandler('xakra_steal:OpenMenu', function(data)
+    local _source = source
+
+    local Amount = 0
+
+    for _, player in pairs(GetPlayers()) do
+        local CharacterJob = Player(player).state.Character and Player(player).state.Character.Job
+
+        for _, job in pairs(Config.RequiredJobs.Jobs) do
+            if CharacterJob == job then
+                Amount = Amount + 1
+                break
+            end
+        end
+    end
+
+    if Amount < Config.RequiredJobs.Amount then
+        VORPcore.NotifyObjective(_source, T.NotJobs, 4000)
+        return
+    end
+
+    Player(_source).state:set('DataSteal', data, true)
+
+    exports.vorp_inventory:closeInventory(data.source)
+
+    Player(data.source).state:set('Stealing', true, true)
+    local Character = VORPcore.getUser(data.source).getUsedCharacter
+    TriggerClientEvent('xakra_steal:OpenMenu', _source, Character.money)
+end)
+
+RegisterServerEvent('xakra_steal:Stealing')
+AddEventHandler('xakra_steal:Stealing', function(steal_source, enable)
+    Player(steal_source).state:set('Stealing', enable, true)
+end)
+
+RegisterServerEvent('xakra_steal:StealMoney')
+AddEventHandler('xakra_steal:StealMoney', function(steal_source, amount)
+    local _source = source
+    local StealCharacter = VORPcore.getUser(steal_source).getUsedCharacter
+
+    if CheckLimit(_source, steal_source, 'Money', amount) then
+        StealCharacter.removeCurrency(0, amount)
+
+        local Character = VORPcore.getUser(_source).getUsedCharacter
+        Character.addCurrency(0, amount)
+
+        VORPcore.NotifyAvanced(_source, T.StealMoney .. ' ' .. amount .. "$", "menu_textures", "log_gang_bag", "COLOR_PURE_WHITE", 2000)
+
+        DiscordLog(string.format(T.WebhooksLang.DiscordStealMoney, amount, GetPlayerName(_source), GetPlayerName(steal_source)))
+    end
+end)
+
+RegisterServerEvent('xakra_steal:ReloadInventory')
+AddEventHandler('xakra_steal:ReloadInventory', function(steal_source, player_source)
+    local _source = player_source or source
+
+    local inventory = {}
+
+    exports.vorp_inventory:getUserInventoryItems(tonumber(steal_source), function(getInventory)
+        for _, v in pairs(getInventory) do
+            table.insert(inventory, v)
+        end
+    end)
+
+    exports.vorp_inventory:getUserInventoryWeapons(tonumber(steal_source), function(getUserWeapons)
+        for _, v in pairs(getUserWeapons) do
+            v.count = 1
+            v.limit = 1
+            v.type = 'item_weapon'
+            table.insert(inventory, v)
+        end
+    end)
+
+    TriggerClientEvent('vorp_inventory:ReloadstealInventory', _source, json.encode({
+        itemList = inventory,
+        action = 'setSecondInventoryItems',
+    }))
+end)
+
+RegisterServerEvent('xakra_steal:OpenInventory')
+AddEventHandler('xakra_steal:OpenInventory', function(steal_source)
+    local _source = source
+    local Character = VORPcore.getUser(steal_source).getUsedCharacter
+
+    TriggerClientEvent('vorp_inventory:OpenstealInventory', _source, T.MenuTitle, Character.charIdentifier)
+
+    -- Config.itemsLimit = { -- OPTIONAL
+    --     weapons = { itemType = "item_weapon", limit = 1 }, -- how many weapons user is allowed to take
+    --     items = { itemType = "item_standard", limit = 2 }, -- how many items user is allowed to take
+    -- }
+
+    -- Config.timeout = 60 -- OPTIONAL in seconds , if enabled when user reaches limits then a timeout is applied so player cant steal for that amount of time if removed then   once limit reached only after restart they can steal again
+
+    -- local _blacklist = {}
+
+    -- for i, v in pairs(Config.ItemsBlackList) do
+    --     _blacklist[v] = true
+    -- end
+
+    -- local data = {
+    --     source = _source, 
+    --     target = steal_source, 
+    --     title = T.MenuTitle,
+    --     blacklist = _blacklist,
+    --     itemsLimit = Config.itemsLimit,
+    --     timeout = Config.timeout,
+    -- }
+
+    -- exports.vorp_inventory:openPlayerInventory(data)
+end)
+
+RegisterServerEvent('syn_search:MoveTosteal')
+AddEventHandler('syn_search:MoveTosteal', function(obj)
+    local _source = source
+
+    local steal_source = Player(_source).state.DataSteal and Player(_source).state.DataSteal.source
+
+    if not steal_source then
+        return
+    end
+
+    local decode_obj = json.decode(obj)
+    decode_obj.number = tonumber(decode_obj.number)
+
+    if decode_obj.type == 'item_standard' and decode_obj.number > 0 and decode_obj.number <= tonumber(decode_obj.item.count) then
+        local canCarrys = exports.vorp_inventory:canCarryItems(steal_source, decode_obj.number)
+        local canCarry = exports.vorp_inventory:canCarryItem(steal_source, decode_obj.item.name, decode_obj.number)
+        if canCarrys and canCarry then
+            exports.vorp_inventory:subItem(_source, decode_obj.item.name, decode_obj.number, decode_obj.item.metadata)
+            exports.vorp_inventory:addItem(steal_source, decode_obj.item.name, decode_obj.number, decode_obj.item.metadata)
+            Wait(100)
+            TriggerEvent('xakra_steal:ReloadInventory', steal_source, _source)
+            DiscordLog(string.format(T.WebhooksLang.DiscordMoveItem, decode_obj.number, decode_obj.item.label, GetPlayerName(_source), GetPlayerName(steal_source)))
+        else
+            VORPcore.NotifyObjective(_source, T.NotStealCarryItems, 4000)
+        end
+
+    elseif decode_obj.type == 'item_weapon' then
+        local canCarry = exports.vorp_inventory:canCarryWeapons(steal_source, 1, nil, decode_obj.item.name)
+        if canCarry then
+            -- exports.vorp_inventory:subWeapon(_source, decode_obj.item.id)
+            exports.vorp_inventory:giveWeapon(steal_source, decode_obj.item.id, _source)
+            Wait(100)
+            TriggerEvent('xakra_steal:ReloadInventory', steal_source, _source)
+            DiscordLog(string.format(T.WebhooksLang.DiscordMoveWeapon, decode_obj.item.label, GetPlayerName(_source), GetPlayerName(steal_source)))
+        else
+            VORPcore.NotifyObjective(_source, T.NotStealCarryWeapon, 4000)
+        end
+    end
+end)
+
+RegisterServerEvent('syn_search:TakeFromsteal')
+AddEventHandler('syn_search:TakeFromsteal', function(obj)
+    local _source = source
+
+    local steal_source = Player(_source).state.DataSteal and Player(_source).state.DataSteal.source
+
+    if not steal_source then
+        return
+    end
+
+    -- Check if duty status checking is enabled
+    local isOnDuty = false
+    if Config.OnDuty.CheckDutyStatus then
+        isOnDuty = exports['ohio_duty']:isPlayerOnDuty(steal_source)
+    end
+
+    local decode_obj = json.decode(obj)
+    decode_obj.number = tonumber(decode_obj.number)
+
+    local inblacklist = false
+    
+    -- Check regular blacklist
+    for _, item in pairs(Config.ItemsBlackList) do
+        if item == decode_obj.item.name then
+            inblacklist = true
+            VORPcore.NotifyObjective(_source, T.ItemInBlackList, 4000)
+        end
+    end
+    
+    -- Check on-duty restricted items
+    if isOnDuty then
+        for _, item in pairs(Config.OnDuty.RestrictedItems) do
+            if item == decode_obj.item.name then
+                inblacklist = true
+                VORPcore.NotifyObjective(_source, T.OnDutyItemRestricted or T.ItemInBlackList, 4000)
+            end
+        end
+    end
+
+    if decode_obj.type == 'item_standard' and not inblacklist and decode_obj.number > 0 and decode_obj.number <= tonumber(decode_obj.item.count) then
+        if not CheckLimit(_source, steal_source, 'Items', decode_obj.number) then
+            return
+        end
+
+        local canCarrys = exports.vorp_inventory:canCarryItems(_source, decode_obj.number)
+        local canCarry = exports.vorp_inventory:canCarryItem(_source, decode_obj.item.name, decode_obj.number)
+        if canCarrys and canCarry then
+            exports.vorp_inventory:subItem(steal_source, decode_obj.item.name, decode_obj.number, decode_obj.item.metadata)
+            exports.vorp_inventory:addItem(_source, decode_obj.item.name, decode_obj.number, decode_obj.item.metadata)
+            Wait(100)
+            TriggerEvent('xakra_steal:ReloadInventory', steal_source, _source)
+            DiscordLog(string.format(T.WebhooksLang.DiscordStealItem, decode_obj.number, decode_obj.item.label, GetPlayerName(_source), GetPlayerName(steal_source)))
+        else
+            VORPcore.NotifyObjective(_source, T.NotCarryItems, 4000)
+        end
+
+    elseif decode_obj.type == 'item_weapon' and not inblacklist then
+        if not CheckLimit(_source, steal_source, 'Weapons', 1) then
+            return
+        end
+
+        local canCarry = exports.vorp_inventory:canCarryWeapons(_source, 1, nil, decode_obj.item.name)
+        if canCarry then
+            -- exports.vorp_inventory:subWeapon(steal_source, decode_obj.item.id)
+            exports.vorp_inventory:giveWeapon(_source, decode_obj.item.id, steal_source)
+            Wait(100)
+            TriggerEvent('xakra_steal:ReloadInventory', steal_source, _source)
+            DiscordLog(string.format(T.WebhooksLang.DiscordStealWeapon, decode_obj.item.label, GetPlayerName(_source), GetPlayerName(steal_source)))
+        else
+            VORPcore.NotifyObjective(_source, T.NotCarryWeapon, 4000)
+        end
+    end
+end)
+
+RegisterServerEvent('xakra_steal:CloseInventory')
+AddEventHandler('xakra_steal:CloseInventory', function()
+    local _source = source
+    exports.vorp_inventory:closeInventory(_source)
+end)
+
+local PlayersLimit = {}
+
+function CheckLimit(source, steal_source, Limit, amount)
+    if Config.Limit[Limit] then
+        local Character = VORPcore.getUser(steal_source).getUsedCharacter
+
+        if not PlayersLimit[Character.charIdentifier] then
+            PlayersLimit[Character.charIdentifier] = {
+                Money = 0,
+                Weapons = 0,
+                Items = 0,
+            }
+        end
+
+        if (PlayersLimit[Character.charIdentifier][Limit] + amount) > Config.Limit[Limit] then
+            VORPcore.NotifyObjective(source, T.Limit .. ' ' .. PlayersLimit[Character.charIdentifier][Limit] .. '/' .. Config.Limit[Limit], 4000)
+            DiscordLog(string.format(T.WebhooksLang.DiscordStealLimit, Limit, GetPlayerName(source), PlayersLimit[Character.charIdentifier][Limit], Config.Limit[Limit]))
+            return false
+        end
+
+        PlayersLimit[Character.charIdentifier][Limit] = PlayersLimit[Character.charIdentifier][Limit] + amount
+
+        return true
+    end
+
+    return true
+end
+
+function DiscordLog(message)
+	if Config.Webhook.UseWebhook then
+		VORPcore.AddWebhook(
+			Config.Webhook.WebhookTitle,
+			Config.Webhook.WebhookUrl,
+			message,
+			Config.Webhook.WebhookColor,
+			Config.Webhook.WebhookName,
+			Config.Webhook.WebhookLogo,
+			Config.Webhook.WebhookLogoFooter,
+			Config.Webhook.WebhookAvatar
+		)
+	end
+end
